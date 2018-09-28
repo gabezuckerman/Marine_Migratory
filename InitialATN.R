1f06209b891fa9d754493a3bf87a2c55f2f98c39
gabezuckerman <gzuckerman@berkeley.edu>
2018-09-27 19:39:42 -0700

    git ignore added

--- InitialATN.R
+++ InitialATN.R
@@ -3,6 +3,6 @@ library(sf)
 library(tidyverse)
 library(lubridate)
 
-atn <- read.csv("ERDDAP_ATN/all_ATN.csv", header = T, stringsAsFactors = F)
+atn <- read.csv("MMA_Data/ERDDAP_ATN/all_ATN.csv", header = T, stringsAsFactors = F)
 atn <- atn[-1,]
 atn$realTime <- ymd_hms(atn$time)
