831f9800f388c952d258b232a48f851df385f46e
gabezuckerman <gzuckerman@berkeley.edu>
2018-09-27 19:26:55 -0700

    initial commit

---
+++ InitialATN.R
@@ -0,0 +1,8 @@
+library(mapview)
+library(sf)
+library(tidyverse)
+library(lubridate)
+
+atn <- read.csv("ERDDAP_ATN/all_ATN.csv", header = T, stringsAsFactors = F)
+atn <- atn[-1,]
+atn$realTime <- ymd_hms(atn$time)
