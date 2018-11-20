fd61b045f07b6206939741c623dddd7b91b276ff
Erin Westeen <ewesteen@Erins-MacBook-Pro-2.local>
2018-11-20 14:43:17 -0700

    minor changes

--- MCP.Fxn.R
+++ MCP.Fxn.R
@@ -6,7 +6,9 @@ library(raster)
 library(adehabitatHR);
 library(dplyr);
 library(RColorBrewer);
-library(gridExtra)
+library(gridExtra);
+library(scales);
+library(maptools)
 
 
 ATN_data <- read.csv("atnPacific_withUTMs.csv", header = TRUE, stringsAsFactors = FALSE)
@@ -31,7 +33,6 @@ plot.mcp <- function(species, individuals, conf){
   for ( i in sequence ){
     for ( j in unique(indiv$UTMzone) ){
       indiv.zone <- indiv[indiv$serialNumber == i & indiv$UTMzone == j ,  ]
-      
       if(nrow(indiv.zone) >= 5){
         idsp <- data.frame(indiv.zone$serialNumber)
         coordinates(idsp) <- cbind(indiv.zone$UTM.east, indiv.zone$UTM.north)
@@ -47,16 +48,17 @@ plot.mcp <- function(species, individuals, conf){
     } 
   }
   
-  ## bind them back together by individual
+  ## bind them back together by individual and create a poly object to plot
   for ( k in sequence ){
     indivPoly <- aggregate(indivPolys[indivPolys$id == k , ])
+    ##indivPoly <- unionSpatialPolygons(indivPolys[indivPolys$id == k , ], IDs = rep(1, sum(indivPolys$id == k)))
+    
     if ( k == sequence[1] ){
       polyToPlot <- indivPoly
     }else{
       polyToPlot <- bind(polyToPlot, indivPoly)
     } 
   } 
-
 
   polyToPlot$id <- sequence
   
@@ -67,12 +69,23 @@ plot.mcp <- function(species, individuals, conf){
     pal <- c(brewer.pal(12, "Paired"),  brewer.pal(extra, "Paired") )
   }
   
-  leaflet(polyToPlot) %>% addTiles() %>% addPolygons(weight = 3, opacity = 0.5, color = pal , fillColor = pal)  %>%  addLegend('bottomleft', colors = pal, labels = sequence,  title = c(unique(focalSp$species)) )
+  leaflet(polyToPlot) %>% addTiles() %>% addPolygons(weight = .3, opacity = 1 , color = pal , fillColor = pal)  %>%  addLegend('bottomleft', colors = pal, labels = sequence,  title = c(unique(focalSp$species)) )
 }
+
+## need to fix map so that it is pacific centered and doesnt break ranges
 
 
 plot.mcp("Leatherback Sea Turtle", 10, 90)
 
 plot.mcp("Laysan Albatross", 5, 75)
 
-plot.mcp("Northern Elephant Seal", 12, 50)
+plot.mcp("Northern Elephant Seal", 12, 100)
+
+
+output$map <- renderLeaflet({
+  leaflet() %>%
+    addProviderTiles(providers$Esri.OceanBasemap) %>%
+    setView(lng = 180, lat = 0, zoom = 2)
+})
+
+
